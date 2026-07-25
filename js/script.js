8fa36f79b18d190c5ba428440a035dc127972069
Shourov-Islam <mrtrepolr44@gmail.com>
2026-07-25 20:07:57 +0600

    solve datatype problem, add minimum range

--- js/script.js
+++ js/script.js
@@ -5,34 +5,42 @@ let submiteBtn = document.querySelector('.submite-btn');
 let input = document.getElementsByTagName('input');
 let massageOutput = document.getElementById('massage-output');
 
+// minimum numbers calculate
+let minimunMidMarks = "";
+let minimumOthersNumber = "";
 // smart placeholder changing
 midTermMarks.addEventListener('input', function(){
+
+    minimumOthersNumber = Math.round((30 - midTermMarks.value*0.25)*100)/100;
+
     if(midTermMarks.value == ""){
         othersNumber.placeholder = "Input others marks";
     } else {
-        othersNumber.placeholder = `minimum ${30 - midTermMarks.value*0.25} marks need`
+        othersNumber.placeholder = `minimum ${minimumOthersNumber} marks need`
     }
 })
 
 othersNumber.addEventListener('input', function(){
+
+     minimunMidMarks = Math.round(((30 - othersNumber.value)*4)*100)/100;
+
     if(othersNumber.value == ""){
         midTermMarks.placeholder = "Input Mid term marks";
     } else {
-        midTermMarks.placeholder = `minimum ${(30 - othersNumber.value)*4} marks need`
+        midTermMarks.placeholder = `minimum ${minimunMidMarks} marks need`
     }
 })
 
 // after submite operation
 submiteBtn.addEventListener('click', function(){
-    let inputMidMarks = midTermMarks.value ;
-    let inputOthersMarks = othersNumber.value ;
-    console.log(inputMidMarks)
-    console.log(inputOthersMarks)
+    let inputMidMarks = Number(midTermMarks.value) || ""; // empty input, keep ""
+    let inputOthersMarks = Number(othersNumber.value) || "" ;
+    
     // empty input then red border show in input filed + Range check
         if (
             (inputMidMarks === '' || inputOthersMarks === '') ||
             (inputMidMarks < 0 || inputMidMarks > 100) ||
-            (inputOthersMarks < 0 || inputOthersMarks > 25 || inputOthersMarks < )
+            (inputOthersMarks < 0 || inputOthersMarks > 25)
         ) {
             for (let c of input) {
                 c.classList.add("input-required");
@@ -45,21 +53,23 @@ submiteBtn.addEventListener('click', function(){
         }
 
         // if not possible to get a+
-        if(inputMidMarks < 20 || inputOthersMarks < 10){
+        if(inputMidMarks < minimunMidMarks || inputOthersMarks < minimumOthersNumber){
             showMarks.innerText = 'SORRY';
             massageOutput.innerText = `you won't get A+ in this subject`
             return;
         }
 
-
-
     
     let finalMarks = 80 - ((inputMidMarks*0.25) + inputOthersMarks);
 
+    console.log(finalMarks);
+
+    showMarks.innerText = `${Math.round(((finalMarks*2)*100))/100} Marks`;
+
     massageOutput.innerText = `need in final exam to get 80 marks or A+ in this subject. Best of Luck!`;
 
-
-    showMarks.innerText = `${finalMarks*2} Marks`;
+    
+    
 
 })
 
